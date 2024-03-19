2042c60c687733eeff491c4f87befe482da2a892
Pavol Hejný <me@pavolhejny.com>
2024-03-19 13:38:07 +0100

    Initial commit
    Delete unnessesary files

---
+++ README.md
@@ -0,0 +1,6 @@
+# 🧮 Web calculator
+
+Calculator which system you should choose when you are creating new web
+
+
+*Note: This is based on Vanilla HTML+JS (no frameworks) intentionally*
---
+++ batch-project-editor.js
@@ -0,0 +1,15 @@
+module.exports = {
+    ignoreWorkflows: [
+        'contributing',
+        'partners',
+        'badges',
+        //'aiGeneratedWallpaperPrepare',
+        //'aiGeneratedWallpaperLand',
+        'aiGeneratedWallpaperTrigger',
+        'aiGeneratedWallpaperHarvest',
+        'aiGeneratedWallpaperPick',
+        'aiGeneratedWallpaperUseInReadme',
+        'aiGeneratedWallpaperUseInGithub',
+        'publishPatchVersion',
+    ],
+};
