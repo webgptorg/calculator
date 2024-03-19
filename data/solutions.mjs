a280677fecb40a84fe5f4777eedeb18e2927f1a3
Pavol Hejny <me@pavolhejny.com>
2024-03-19 16:25:14 +0100

    Boilerplates

--- data/solutions.mjs
+++ data/solutions.mjs
@@ -1,15 +1,229 @@
 export const solutions = [
     {
-        name: "Vlastní vývoj",
-        description: "We build custom software solutions that are tailored to your business needs.",
+        name: 'Vývoj na míru po vlastní ose',
+        description:
+            'Vývoj na míru po vlastní ose je nejlepší volbou pro ty, kteří chtějí mít naprostou kontrolu nad všemi aspekty svého webu na úkor času a peněz.',
 
-        requirements:[
-            
-        ],
-        features:{
+        fits: 'ALL',
+        requirements: [],
+        features: {},
+    },
+    {
+        name: 'ChatGPT',
+        description: 'ChatGPT můžete využít pro vytvoření jednoduchého webu nebo pro psaní textů.',
+        fits: [''],
+        requirements: [],
+        features: {},
+        limitations: ['Pomocí ChatGPT bez dalších znalostí vytvoříte pouze limitovaný web'],
+    },
+    {
+        name: 'Wordpress' /* po vlastní ose */,
+        description: 'Využijte nejrozšířenější open-source CMS na světě pro vytvoření svých webových stránek.',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: 'Webgpt',
+        description: 'Je systém, který vám umožní vytvořit web pomocí GPT-4 a Dalle-3.',
+        features: {},
+        requirements: {},
+        limitations: ['Pouze limitovaná sada prvků', 'Pouze limitovaná sada designů'],
+    },
+    {
+        name: 'Poptat webovou agenturu',
+        description: '',
 
-        }
-        ,
-       
-    }
-]
+        requirements: [],
+        features: {},
+    },
+    {
+        name: 'Poptat freelancera',
+        description: '',
+
+        requirements: [],
+        features: {},
+    },
+    {
+        name: 'WebFlow',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: 'Wix',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: 'SolidPixels',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: 'Shopify',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: 'Shoptet',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: 'Linktr.ee',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: 'Facebook',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    ,
+    {
+        name: 'LinkedIn',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: 'Instagram',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+
+    // ------------------------------------------ Boilerplate
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+    {
+        name: '',
+        description: '',
+        features: {},
+        requirements: {},
+    },
+];
