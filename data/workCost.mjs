d66f2e920e2e8daf93a6d1692fbda63fb3f0dbe4
Pavol Hejny <me@pavolhejny.com>
2024-03-20 12:36:14 +0100

    Update HTML and JavaScript code for input range elements

---
+++ data/workCost.mjs
@@ -0,0 +1,5 @@
+export const workCost = {
+    initial: '50 USD',
+    design: '20 USD',
+    programming: '30 USD',
+};
--- script/main.mjs
+++ script/main.mjs
@@ -1,9 +1,27 @@
-import {solutions} from'../data/solutions.mjs';
+import { solutions } from '../data/solutions.mjs';
 
-
-
-
-
-export function main(){
+export function main() {
     console.log(solutions);
+
+    for (const inputElement of Array.from(document.querySelectorAll(`input[type="range"][data-show-output]`))) {
+        inputElement.addEventListener('input', (event) => {
+            const value = parseFloat(inputElement.value);
+            const showOutput = inputElement.dataset.showOutput;
+            const formattedValue = value.toLocaleString(
+                'cs-CZ',
+                {
+                    scalar: {
+                        minimumFractionDigits: 0,
+                    },
+                    CZK: {
+                        style: 'currency',
+                        currency: 'CZK',
+                        minimumFractionDigits: 0,
+                    },
+                }[showOutput],
+            );
+
+            event.target.nextElementSibling.value = formattedValue;
+        });
+    }
 }
